6a1c40d56b081ff19d697fd8d462e3f09cec28f5
Sudeep <sdpjswl1@gmail.com>
2022-01-26 15:16:39 +0530

    Updated project settings and fixed warnings

--- Example/ASJColoredScrollIndicatorsExample/UIColor+Radiohead.m
+++ Example/ASJColoredScrollIndicatorsExample/UIColor+Radiohead.m
@@ -12,48 +12,48 @@
 
 + (UIColor *)trueLoveWaitsBackgroundColor
 {
-  return [UIColor colorWithR:255.0f g:0.0f b:128.0f a:0.1f];
+    return [UIColor colorWithR:255.0f g:0.0f b:128.0f a:0.1f];
 }
 
 + (UIColor *)trueLoveWaitsIndicatorColor
 {
-  return [UIColor colorWithR:255.0f g:0.0f b:128.0f a:1.0f];
+    return [UIColor colorWithR:255.0f g:0.0f b:128.0f a:1.0f];
 }
 
 + (UIColor *)okComputerTableBackgroundColor
 {
-  return [UIColor colorWithR:241.0f g:242.0f b:245.0f a:0.1f];
+    return [UIColor colorWithR:241.0f g:242.0f b:245.0f a:0.1f];
 }
 
 + (UIColor *)okComputerIndicatorColor
 {
-  return [UIColor colorWithR:80.0f g:109.0f b:132.0f a:1.0f];
+    return [UIColor colorWithR:80.0f g:109.0f b:132.0f a:1.0f];
 }
 
 + (UIColor *)inRainbowsTableBackgroundColor
 {
-  return [UIColor colorWithWhite:0.2f alpha:1.0f];
+    return [UIColor colorWithWhite:0.2f alpha:1.0f];
 }
 
 + (UIColor *)inRainbowsIndicatorColor
 {
-  return [UIColor orangeColor];
+    return [UIColor orangeColor];
 }
 
 + (UIColor *)radioheadAlbumsBackgroundColor
 {
-  return [UIColor colorWithR:35.0f g:43.0f b:8.0f a:0.1f];
+    return [UIColor colorWithR:35.0f g:43.0f b:8.0f a:0.1f];
 }
 
 + (UIColor *)radioheadAlbumsIndicatorColor
 {
-  return [UIColor colorWithR:0.0f g:122.0f b:255.0f a:1.0f];
-  return [UIColor colorWithR:35.0f g:90.0f b:8.0f a:1.0f];
+    return [UIColor colorWithR:0.0f g:122.0f b:255.0f a:1.0f];
+    return [UIColor colorWithR:35.0f g:90.0f b:8.0f a:1.0f];
 }
 
 + (UIColor *)colorWithR:(CGFloat)r g:(CGFloat)g b:(CGFloat)b a:(CGFloat)a
 {
-  return [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a];
+    return [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a];
 }
 
 @end
--- Example/ASJColoredScrollIndicatorsExample/ViewController.m
+++ Example/ASJColoredScrollIndicatorsExample/ViewController.m
@@ -38,166 +38,201 @@ static NSString *const kRadioheadAlbumsCellIdentifier = @"radiohead_albums_cell_
 
 - (void)viewDidLoad
 {
-  [super viewDidLoad];
-  [self setup];
+    [super viewDidLoad];
+    [self setup];
 }
 
 - (void)didReceiveMemoryWarning
 {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Setup
 
 - (void)setup
 {
-  self.automaticallyAdjustsScrollViewInsets = NO;
-  [self setupTextView];
-  [self setupTableViews];
-  [self setupCollectionViews];
+    [self setupTextView];
+    [self setupTableViews];
+    [self setupCollectionViews];
 }
 
 - (void)setupTextView
 {
-  _trueLoveWaitsTextView.editable = NO;
-  _trueLoveWaitsTextView.selectable = NO;
-  _trueLoveWaitsTextView.backgroundColor = [UIColor trueLoveWaitsBackgroundColor];
-  _trueLoveWaitsTextView.scrollIndicatorColor = [UIColor trueLoveWaitsIndicatorColor];
+    _trueLoveWaitsTextView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
+    _trueLoveWaitsTextView.editable = NO;
+    _trueLoveWaitsTextView.selectable = NO;
+    _trueLoveWaitsTextView.backgroundColor = [UIColor trueLoveWaitsBackgroundColor];
+    _trueLoveWaitsTextView.scrollIndicatorColor = [UIColor trueLoveWaitsIndicatorColor];
 }
 
 - (void)setupTableViews
 {
-  _okComputerTableView.tag = 0;
-  _okComputerTableView.backgroundColor = [UIColor okComputerTableBackgroundColor];
-  _okComputerTableView.scrollIndicatorColor = [UIColor okComputerIndicatorColor];
-  [_okComputerTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kOKComputerCellIdentifier];
-  
-  _inRainbowsTableView.tag = 1;
-  _inRainbowsTableView.backgroundColor = [UIColor inRainbowsTableBackgroundColor];
-  _inRainbowsTableView.scrollIndicatorColor = [UIColor inRainbowsIndicatorColor];
-  [_inRainbowsTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kInRainbowsCellIdentifier];
+    _okComputerTableView.tag = 0;
+    _okComputerTableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
+    _okComputerTableView.backgroundColor = [UIColor okComputerTableBackgroundColor];
+    _okComputerTableView.scrollIndicatorColor = [UIColor okComputerIndicatorColor];
+    [_okComputerTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kOKComputerCellIdentifier];
+    
+    _inRainbowsTableView.tag = 1;
+    _inRainbowsTableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
+    _inRainbowsTableView.backgroundColor = [UIColor inRainbowsTableBackgroundColor];
+    _inRainbowsTableView.scrollIndicatorColor = [UIColor inRainbowsIndicatorColor];
+    [_inRainbowsTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kInRainbowsCellIdentifier];
 }
 
 - (void)setupCollectionViews
 {
-  _radioheadAlbumsCollectionView.backgroundColor = [UIColor radioheadAlbumsBackgroundColor];
-  _radioheadAlbumsCollectionView.scrollIndicatorColor = [UIColor radioheadAlbumsIndicatorColor];
+    _radioheadAlbumsCollectionView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
+    _radioheadAlbumsCollectionView.backgroundColor = [UIColor radioheadAlbumsBackgroundColor];
+    _radioheadAlbumsCollectionView.scrollIndicatorColor = [UIColor radioheadAlbumsIndicatorColor];
 }
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-  if (!tableView.tag) {
-    return self.okComputerTableData.count;
-  }
-  return self.inRainbowsTableData.count;
+    if (!tableView.tag) {
+        return self.okComputerTableData.count;
+    }
+    return self.inRainbowsTableData.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  UITableViewCell *cell = nil;
-  
-  if (tableView.tag == 0)
-  {
-    cell = [tableView dequeueReusableCellWithIdentifier:kOKComputerCellIdentifier forIndexPath:indexPath];
-    cell.textLabel.textColor = [UIColor okComputerIndicatorColor];
-    cell.textLabel.text = self.okComputerTableData[indexPath.row];
-  }
-  else
-  {
-    cell = [tableView dequeueReusableCellWithIdentifier:kInRainbowsCellIdentifier forIndexPath:indexPath];
-    cell.textLabel.textColor = [UIColor inRainbowsIndicatorColor];
-    cell.textLabel.text = self.inRainbowsTableData[indexPath.row];
-  }
-  
-  cell.selectionStyle = UITableViewCellSelectionStyleNone;
-  cell.backgroundColor = [UIColor clearColor];
-  cell.textLabel.font = [UIFont systemFontOfSize:13.0f];
-  
-  return cell;
+    UITableViewCell *cell = nil;
+    
+    if (tableView.tag == 0)
+    {
+        cell = [tableView dequeueReusableCellWithIdentifier:kOKComputerCellIdentifier forIndexPath:indexPath];
+        cell.textLabel.textColor = [UIColor okComputerIndicatorColor];
+        cell.textLabel.text = self.okComputerTableData[indexPath.row];
+    }
+    else
+    {
+        cell = [tableView dequeueReusableCellWithIdentifier:kInRainbowsCellIdentifier forIndexPath:indexPath];
+        cell.textLabel.textColor = [UIColor inRainbowsIndicatorColor];
+        cell.textLabel.text = self.inRainbowsTableData[indexPath.row];
+    }
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.backgroundColor = [UIColor clearColor];
+    cell.textLabel.font = [UIFont systemFontOfSize:13.0f];
+    
+    return cell;
 }
 
 #pragma mark - Table data properties
 
 - (NSArray *)okComputerTableData
 {
-  return @[@"Airbag",
-           @"Paranoid Android",
-           @"Subterranean Homesick Alien",
-           @"Exit Music (For a Film)",
-           @"Let Down",
-           @"Karma Police",
-           @"Fitter Happier",
-           @"Electioneering",
-           @"Climbing Up the Walls",
-           @"No Surprises",
-           @"Lucky",
-           @"The Tourist"];
+    return @[@"Airbag",
+             @"Paranoid Android",
+             @"Subterranean Homesick Alien",
+             @"Exit Music (For a Film)",
+             @"Let Down",
+             @"Karma Police",
+             @"Fitter Happier",
+             @"Electioneering",
+             @"Climbing Up the Walls",
+             @"No Surprises",
+             @"Lucky",
+             @"The Tourist",
+             @"Airbag",
+             @"Paranoid Android",
+             @"Subterranean Homesick Alien",
+             @"Exit Music (For a Film)",
+             @"Let Down",
+             @"Karma Police",
+             @"Fitter Happier",
+             @"Electioneering",
+             @"Climbing Up the Walls",
+             @"No Surprises",
+             @"Lucky",
+             @"The Tourist"];
 }
 
 - (NSArray *)inRainbowsTableData
 {
-  return @[@"15 Step",
-           @"Bodysnatchers",
-           @"Nude",
-           @"Weird Fishes / Arpeggi",
-           @"All I Need",
-           @"Faust Arp",
-           @"Reckoner",
-           @"House of Cards",
-           @"Jigsaw Falling Into Place",
-           @"Videotape"];
+    return @[@"15 Step",
+             @"Bodysnatchers",
+             @"Nude",
+             @"Weird Fishes / Arpeggi",
+             @"All I Need",
+             @"Faust Arp",
+             @"Reckoner",
+             @"House of Cards",
+             @"Jigsaw Falling Into Place",
+             @"Videotape",
+             @"15 Step",
+             @"Bodysnatchers",
+             @"Nude",
+             @"Weird Fishes / Arpeggi",
+             @"All I Need",
+             @"Faust Arp",
+             @"Reckoner",
+             @"House of Cards",
+             @"Jigsaw Falling Into Place",
+             @"Videotape"];
 }
 
 #pragma mark - UITableViewDelegate
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  return 33.0f;
+    return 33.0f;
 }
 
 #pragma mark - UICollectionViewDataSource
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-  return self.radioheadAlbumsCollectionViewData.count;
+    return self.radioheadAlbumsCollectionViewData.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-  UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kRadioheadAlbumsCellIdentifier forIndexPath:indexPath];
-  
-  UIImageView *imageView = (UIImageView *)[cell viewWithTag:1];
-  NSString *imageName = self.radioheadAlbumsCollectionViewData[indexPath.row];
-  imageView.image = [UIImage imageNamed:imageName];
-  
-  return cell;
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kRadioheadAlbumsCellIdentifier forIndexPath:indexPath];
+    
+    UIImageView *imageView = (UIImageView *)[cell viewWithTag:1];
+    NSString *imageName = self.radioheadAlbumsCollectionViewData[indexPath.row];
+    imageView.image = [UIImage imageNamed:imageName];
+    
+    return cell;
 }
 
 - (NSArray *)radioheadAlbumsCollectionViewData
 {
-  return @[@"pablo_honey",
-           @"the_bends",
-           @"ok_computer",
-           @"kid_a",
-           @"amnesiac",
-           @"hail_to_the_thief",
-           @"in_rainbows",
-           @"the_king_of_limbs",
-           @"a_moon_shaped_pool",
-           @"logo"];
+    return @[@"pablo_honey",
+             @"the_bends",
+             @"ok_computer",
+             @"kid_a",
+             @"amnesiac",
+             @"hail_to_the_thief",
+             @"in_rainbows",
+             @"the_king_of_limbs",
+             @"a_moon_shaped_pool",
+             @"logo",
+             @"pablo_honey",
+             @"the_bends",
+             @"ok_computer",
+             @"kid_a",
+             @"amnesiac",
+             @"hail_to_the_thief",
+             @"in_rainbows",
+             @"the_king_of_limbs",
+             @"a_moon_shaped_pool",
+             @"logo"];
 }
 
 #pragma mark - IBAction
 
 - (IBAction)flashIndicators:(id)sender
 {
-  [_trueLoveWaitsTextView flashScrollIndicators];
-  [_okComputerTableView flashScrollIndicators];
-  [_inRainbowsTableView flashScrollIndicators];
-  [_radioheadAlbumsCollectionView flashScrollIndicators];
+    [_trueLoveWaitsTextView flashScrollIndicators];
+    [_okComputerTableView flashScrollIndicators];
+    [_inRainbowsTableView flashScrollIndicators];
+    [_radioheadAlbumsCollectionView flashScrollIndicators];
 }
 
 @end
